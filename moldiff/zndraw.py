95ea249ffdaf89bc0901238c63837ea49c943190
Rokas Elijošius <re344@cam.ac.uk>
2023-05-31 15:59:51 +0100

    combined the three previous zndraw classes into one for speeed

--- moldiff/zndraw.py
+++ moldiff/zndraw.py
@@ -4,14 +4,13 @@ import ase
 import networkx as nx
 import numpy as np
 import torch
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, PrivateAttr
 
 from moldiff.atoms_cleanup import (
     attach_calculator,
     relax_hydrogens,
     run_dynamics,
 )
-from moldiff.calculators import MaceSimilarityCalculator
 from moldiff.diffusion_tools import SamplerNoiseParameters
 from moldiff.element_swapping import SwappingAtomicNumberTable
 from moldiff.generation_utils import (
@@ -34,10 +33,10 @@ class UpdateScene(BaseModel, abc.ABC):
         pass
 
 
-def load_mace_calc():
-    pretrained_mace_path = "/home/rokas/Programming/Generative_model_energy/models/SPICE_sm_inv_neut_E0_swa.model"
+def load_mace_calc(model_path, reference_data_path):
+    pretrained_mace_path = model_path
     rng = np.random.default_rng(0)
-    data_path = "/home/rokas/Programming/data/qm9_reference_data.xyz"
+    data_path = reference_data_path
     mace_calc = get_mace_similarity_calculator(
         pretrained_mace_path,
         data_path,
@@ -48,53 +47,10 @@ def load_mace_calc():
     return mace_calc
 
 
-def try_to_get_calc(atoms):
-    if atoms.calc is not None and isinstance(atoms.calc, MaceSimilarityCalculator):
-        calc = atoms.calc
-        print("Using preloaded MACE calculator")
-    else:
-        calc = load_mace_calc()
-        print("Loaded MACE calculator")
-    return calc
-
-
 noise_params = SamplerNoiseParameters(
     sigma_max=10, sigma_min=2e-3, S_churn=1.3, S_min=2e-3, S_noise=0.5
 )
 swapping_z_table = SwappingAtomicNumberTable([6, 7, 8], [1, 1, 1])
-
-
-class ConstrainedGeneration(UpdateScene):
-    num_atoms: int = Field(5, ge=1, le=30, description="Number of atoms to generate")
-
-    def run(self, atom_ids: list[int], atoms: ase.Atoms, **kwargs) -> list[ase.Atoms]:
-        mace_calc = try_to_get_calc(atoms)
-        prior = PointCloudPrior(kwargs["points"], beta=5.0)
-        restorative_force_strength = calculate_restorative_force_strength(
-            self.num_atoms
-        )
-        generator = ParticleFilterGenerator(
-            mace_calc,
-            prior,
-            noise_params=noise_params,
-            device=DEVICE,
-            restorative_force_strength=restorative_force_strength,
-        )
-        mol = ase.Atoms(f"C{self.num_atoms}")
-        mol = prior.initialise_positions(mol, scale=0.5)
-        molecule, mask, torch_mask = generator._merge_scaffold_and_create_mask(
-            mol, atoms, num_particles=10, device=DEVICE
-        )
-        trajectories = generator._maximise_log_similarity(
-            molecule,
-            particle_swap_frequency=4,
-            num_particles=10,
-            swapping_z_table=swapping_z_table,
-            mask=mask,
-            torch_mask=torch_mask,
-        )
-        trajectories[-1].calc = mace_calc
-        return trajectories
 
 
 def hydrogenate(atoms, graph_representation):
@@ -111,13 +67,94 @@ def hydrogenate(atoms, graph_representation):
     return atoms_with_hs
 
 
-class Hydrogenate(UpdateScene):
-    num_steps: int = Field(
-        10, ge=1, le=100, description="Number of hydrogen relaxtion steps"
+class MoldiffGeneration(UpdateScene):
+    model_path: str = Field(
+        "/home/rokas/Programming/Generative_model_energy/models/SPICE_sm_inv_neut_E0_swa.model"
     )
+    reference_data_path: str = Field(
+        "/home/rokas/Programming/data/qm9_reference_data.xyz"
+    )
+    num_atoms_to_add: int = Field(
+        5, ge=1, le=30, description="Number of atoms to generate"
+    )
+    relaxation_steps: int = Field(
+        10, ge=1, le=100, description="Number of relaxation steps"
+    )
+    run_type: str = Field("generate", description="Type of operation to do")
+    _calc = PrivateAttr(None)
 
     def run(self, atom_ids: list[int], atoms: ase.Atoms, **kwargs) -> list[ase.Atoms]:
-        calc = try_to_get_calc(atoms)
+        if self._calc is None:
+            print("Initializing MACE calculator")
+            self._calc = load_mace_calc(
+                kwargs["model_path"], kwargs["reference_data_path"]
+            )
+        else:
+            print("Using existing MACE calculator")
+
+        if "run_type" in kwargs:
+            run_type = kwargs["run_type"].strip().lower()
+        else:
+            run_type = self.run_type.strip().lower()
+
+        if run_type == "generate":
+            return self._generate(atom_ids, atoms, **kwargs)
+        elif run_type == "hydrogenate":
+            return self._hydrogenate(atom_ids, atoms, **kwargs)
+        elif run_type == "relax":
+            return self._relax(atom_ids, atoms, **kwargs)
+        else:
+            print(
+                f"Unrecognized `run_type`:f{run_type}\nShould be one of: `generate`, `hydrogenate`, `relax`"
+            )
+            return [atoms]
+
+    def _generate(
+        self, atom_ids: list[int], atoms: ase.Atoms, **kwargs
+    ) -> list[ase.Atoms]:
+        calc = self._calc
+        prior = PointCloudPrior(kwargs["points"], beta=5.0)
+        num_atoms_to_add = (
+            kwargs["num_atoms_to_add"]
+            if "num_atoms_to_add" in kwargs
+            else self.num_atoms_to_add
+        )
+        num_atoms_to_add = int(num_atoms_to_add)
+        restorative_force_strength = calculate_restorative_force_strength(
+            num_atoms_to_add
+        )
+        generator = ParticleFilterGenerator(
+            calc,
+            prior,
+            noise_params=noise_params,
+            device=DEVICE,
+            restorative_force_strength=restorative_force_strength,
+        )
+        mol = ase.Atoms(f"C{num_atoms_to_add}")
+        mol = prior.initialise_positions(mol, scale=0.5)
+        molecule, mask, torch_mask = generator._merge_scaffold_and_create_mask(
+            mol, atoms, num_particles=10, device=DEVICE
+        )
+        trajectories = generator._maximise_log_similarity(
+            molecule,
+            particle_swap_frequency=4,
+            num_particles=10,
+            swapping_z_table=swapping_z_table,
+            mask=mask,
+            torch_mask=torch_mask,
+        )
+        return trajectories
+
+    def _hydrogenate(
+        self, atom_ids: list[int], atoms: ase.Atoms, **kwargs
+    ) -> list[ase.Atoms]:
+        calc = self._calc
+        relaxation_steps = (
+            kwargs["relaxation_steps"]
+            if "relaxation_steps" in kwargs
+            else self.relaxation_steps
+        )
+        relaxation_steps = int(relaxation_steps)
         try:
             connectivity_graph = atoms.info["graph_representation"]
         except KeyError:
@@ -125,20 +162,25 @@ class Hydrogenate(UpdateScene):
                 "No graph representation found, try resetting the scene of clicking `Save` in the `Bonds` tab"
             )
             return [atoms]
+        except Exception as e:
+            raise e
         hydrogenated_atoms = hydrogenate(atoms, connectivity_graph)
         relaxed_hydrogenated_atoms = hydrogenated_atoms.copy()
         relaxed_hydrogenated_atoms = relax_hydrogens(
-            [relaxed_hydrogenated_atoms], calc, num_steps=self.num_steps, max_step=0.1
+            [relaxed_hydrogenated_atoms], calc, num_steps=relaxation_steps, max_step=0.1
         )[0]
-        relaxed_hydrogenated_atoms.calc = calc
         return [hydrogenated_atoms, relaxed_hydrogenated_atoms]
 
-
-class Relax(UpdateScene):
-    num_steps: int = Field(10, ge=1, le=100, description="Number of calculation steps")
-
-    def run(self, atom_ids: list[int], atoms: ase.Atoms, **kwargs) -> list[ase.Atoms]:
-        calc = try_to_get_calc(atoms)
+    def _relax(
+        self, atom_ids: list[int], atoms: ase.Atoms, **kwargs
+    ) -> list[ase.Atoms]:
+        calc = self._calc
+        relaxation_steps = (
+            kwargs["relaxation_steps"]
+            if "relaxation_steps" in kwargs
+            else self.relaxation_steps
+        )
+        relaxation_steps = int(relaxation_steps)
         original_atoms = atoms.copy()
         if len(atom_ids) != 0:
             print("Will relax only the selected atoms")
@@ -151,6 +193,5 @@ class Relax(UpdateScene):
         relaxed_atoms = attach_calculator(
             [atoms], calc, calculation_type="mace", mask=mask
         )
-        relaxed_atoms = run_dynamics(relaxed_atoms, num_steps=self.num_steps)
-        relaxed_atoms[-1].calc = calc
+        relaxed_atoms = run_dynamics(relaxed_atoms, num_steps=relaxation_steps)
         return [original_atoms, *relaxed_atoms]
